097031a0ebc44a3e6f44f6aaaa96b86ab94e8101
Frank Bardon Jr <frank@nerdsrescue.me>
2012-12-07 10:15:26 -0500

    Basic bootstrapping

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,3 +1,25 @@
 <?php
 
-require '../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
+
+use Nerd\Kernel\Kernel
+  , Nerd\Event\Listener
+  , Nerd\Event\Dispatcher
+  , Nerd\Bundle\Manager as BundleManager
+  , Nerd\Container\Container
+  , Nerd\Bundle\Bundle;
+
+$bundle  = new Bundle();
+$manager = new BundleManager();
+    $manager->register($bundle);
+
+$dispatcher = new Dispatcher();
+    $dispatcher->register(Kernel::START, new Listener());
+    $dispatcher->dispatch(Kernel::START);
+
+$container = new Container();
+
+$kernel = new Kernel($dispatcher, $manager, $container);
+$kernel->setRoot(dirname(__DIR__));
+
+return $kernel;
--- public/development.php
+++ public/development.php
@@ -1,26 +1,5 @@
 <?php
 
-require '../application/bootstrap.php';
-
-use Nerd\Kernel\Kernel;
-use Nerd\Event\Listener;
-use Nerd\Event\Dispatcher;
-use Nerd\Bundle\Manager as BundleManager;
-use Nerd\Kernel\Events as KernelEvents;
-use Nerd\Container\Container;
-use Nerd\Bundle\Bundle;
-
-$bundle  = new Bundle();
-$manager = new BundleManager();
-    $manager->register($bundle);
-
-$dispatcher = new Dispatcher();
-    $dispatcher->register(Kernel::START, new Listener());
-    $dispatcher->dispatch(Kernel::START);
-
-$container = new Container();
-
-$kernel = new Kernel($dispatcher, $manager, $container);
-$kernel->setRoot(realpath(__DIR__.'/../'));
+$kernel = require '../application/bootstrap.php';
 
 die(var_dump($kernel));
