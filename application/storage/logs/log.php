52f0b612d5be3bf377ea535ecd8fc0d483ec18f0
Frank Bardon Jr <frank@nerdsrescue.me>
2013-01-08 15:53:01 -0500

    Updating editor views

---
+++ application/storage/logs/log.php
@@ -0,0 +1,6 @@
+[2013-01-08 15:46:54] App.ERROR: Action [mainAction] does not exist in controller
+	#0 C:\XAMPP\htdocs\di\vendor\nerdsrescueme\core\Nerd\Core\Event\Event.php(92): CMS\Event\ResponsePathObserver->update(Object(Nerd\Core\Event\Event))
+	#1 C:\XAMPP\htdocs\di\public\development.php(68): Nerd\Core\Event\Event->notify()
+	#2 C:\XAMPP\htdocs\di\public\index.php(3): include('C:\XAMPP\htdocs...')
+	#3 {main}
+	 ["C:\\XAMPP\\htdocs\\di\\application\\src\\CMS\\Event\\ResponsePathObserver.php",32] []
